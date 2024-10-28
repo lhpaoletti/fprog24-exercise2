19a3e7d43899d848190c703430a077dd9b41bd73
Luis Henrique Paoletti <lh.paoletti@gmail.com>
2024-10-28 23:20:58 +0100

    Implemented A3

--- src/main.hs
+++ src/main.hs
@@ -6,3 +6,18 @@ module Exercise2 where
 import Types
 import A1
 import A2
+
+{- Whether a list and a list' are equal by these conditions:
+ - 1. They have the same length
+ - 2. They are equal element-wise
+ -}
+eq :: List -> List' -> Bool
+eq E E' = True
+eq E _  = False
+eq _ E' = False  -- this line makes it safe to use fst and shift bellow
+eq (L e l) l' = e == fst l'
+                && eq l (shift l')
+  where fst (L' E' x) = x     -- get first element of a non-empty List'
+        fst (L' xs _) = fst xs
+        shift (L' E' _) = E'  -- shift a non-empty List' to the left, "cutting" the first element
+        shift (L' xs x) = L' (shift xs) x
